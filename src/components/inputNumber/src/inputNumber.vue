451ab96c337ec276650fad016022493b26560b45
sudong.duan <1006364428@qq.com>
2021-04-22 17:38:22 +0800

    <feat>:inputNumber组件开发完成

--- src/components/inputNumber/src/inputNumber.vue
+++ src/components/inputNumber/src/inputNumber.vue
@@ -1,14 +1,24 @@
 // 计数器组件
 <template>
-    <div :class="`cs-inputNumber ${(isFocus && !disabled) ? 'cs-inputNumber-focus' : ''} ${disabled ? 'cs-inputNumber-disabled' : ''}`" :style="`border-radius: ${computedBorderRadius}`">
-        <div :class="`cs-inputNumber-less-default ${(isDisabledLess || disabled) ? 'cs-inputNumber-less-default-disabled' : ''}`" @click="less">
+    <div :class="`cs-inputNumber ${(isFocus && !disabled) ? 'cs-inputNumber-focus' : ''} ${disabled ? 'cs-inputNumber-disabled' : ''} ${size ? 'cs-inputNumber-'+size : ''}`" :style="`border-radius: ${computedBorderRadius}`">
+        <div v-if="controlsPosition == 'default'" :class="`cs-inputNumber-less-default ${(isDisabledLess || disabled) ? 'cs-inputNumber-less-default-disabled' : ''} ${size ? 'cs-inputNumber-control-'+size : ''}`" @click="less">
             <i class="cs-icon-less"></i>
         </div>
-        <div :class="`cs-inputNumber-add-default ${(isDisabledAdd || disabled) ? 'cs-inputNumber-add-default-disabled' : ''}`" @click="add">
+        <div v-if="controlsPosition == 'default'" :class="`cs-inputNumber-add-default ${(isDisabledAdd || disabled) ? 'cs-inputNumber-add-default-disabled' : ''} ${size ? 'cs-inputNumber-control-'+size : ''}`" @click="add">
             <i class="cs-icon-add"></i>
         </div>
-        <div :class="`cs-inputNumber-content cs-inputNumber-content-${controlsPosition} ${disabled ? 'cs-inputNumber-content-disabled' : ''}`">
-            <csInput v-model="inputValue" :disabled="disabled" @focus="focus" @blur="blur" @change="change" type="number"></csInput>
+
+        <div v-if="controlsPosition == 'right'" :class="`cs-inputNumber-add-default ${controlsPosition == 'right' ? 'cs-inputNumber-control-box' : ''} ${size ? 'cs-inputNumber-add-default-'+size : ''}` ">
+            <div :class="`cs-inputNumber-control-add ${(isDisabledAdd || disabled) ? 'cs-inputNumber-add-default-disabled' : ''} ${size ? 'cs-inputNumber-control-'+size : ''}`" @click="add">
+                <i class="cs-icon-fold"></i>
+            </div>
+            <div :class="`cs-inputNumber-control-less ${(isDisabledLess || disabled) ? 'cs-inputNumber-less-default-disabled' : ''} ${size ? 'cs-inputNumber-control-'+size : ''}`" @click="less">
+                <i class="cs-icon-unfold"></i>
+            </div>
+        </div>
+
+        <div :class="`cs-inputNumber-content cs-inputNumber-content-${controlsPosition} ${disabled ? 'cs-inputNumber-content-disabled' : ''} ${size ? 'cs-inputNumber-content-'+size : ''}`">
+            <csInput v-model="inputValue" :readonly="disabledInput" :disabled="disabled" :size="size" @focus="focus" @blur="blur" @change="change" :type="computedInputType" :toFixed="precision"></csInput>
         </div>
     </div>
 </template>
@@ -34,7 +44,7 @@ export default {
         },
         controlsPosition: {   //控制选项的位置
             type: String,
-            default: 'default'   //left default right
+            default: 'default'   //default right
         },
         step: {   //步长
             type: Number | String,
@@ -42,15 +52,27 @@ export default {
         },
         max: {   //最大值
             type: String | Number,
-            default: '10'
+            default: ''
         },
         min: {   //最小值
             type: String | Number,
-            default: '0'
+            default: ''
         },
         disabled: {   //是否禁用
             type: Boolean,
             default: false
+        },
+        disabledInput: {   //禁止输入
+            type: Boolean,
+            default: false
+        },
+        precision: {   //精度
+            type: Number | String,
+            default: 0
+        },
+        size: {    //large、small、mini
+            type: String,
+            default: ''
         }
     },
     components: {
@@ -60,14 +82,30 @@ export default {
         prop: 'value',
         event: 'change'
     },
+    mounted() {
+        const num = 0, value = new Number(this.value), precision = Number(this.precision)
+        this.inputValue = num.toFixed.call(value, precision)
+    },
     computed: {
+        /**
+         * 计算输入框类型
+         * @param
+         * @return {String} 输入框类型
+         */
+        computedInputType() {
+            if (this.min !== '' && this.min >= 0) {
+                return 'positive'
+            } else {
+                return 'number'
+            }
+        },
         /**
          * 计算圆角值
          * @param
          * @return {String} 圆角属性
          */
         computedBorderRadius() {
-            if(typeof this.borderRadius === 'number') {
+            if (typeof this.borderRadius === 'number') {
                 return `${this.borderRadius}px`
             } else {
                 return this.borderRadius
@@ -79,10 +117,10 @@ export default {
          * @return {Boolean} 是否禁用减少按钮
          */
         isDisabledLess() {
-            if(this.min === '') {
+            if (this.min === '') {
                 return false
             }
-            if(Number(this.inputValue) <= Number(this.min)) {
+            if (Number(this.inputValue) <= Number(this.min)) {
                 return true
             } else {
                 return false
@@ -94,10 +132,10 @@ export default {
          * @return {Boolean} 是否禁用增加按钮
          */
         isDisabledAdd() {
-            if(this.max === '') {
+            if (this.max === '') {
                 return false
             }
-            if(Number(this.inputValue) >= Number(this.max)) {
+            if (Number(this.inputValue) >= Number(this.max)) {
                 return true
             } else {
                 return false
@@ -122,7 +160,14 @@ export default {
         blur() {
             this.isFocus = false
             this.$emit('blur')
-
+            let result = Math.round(this.inputValue / this.step) * this.step
+            result = (this.max !== '' && result > this.max) ? this.max : result
+            result = (this.min !== '' && result < this.min) ? this.min : result
+            if (result != this.inputValue) {
+                result = result.toFixed(Number(this.precision))
+                this.inputValue = result
+                this.$emit('change', result)
+            }
         },
         /**
          * 点击加号时触发
@@ -130,13 +175,14 @@ export default {
          * @return
          */
         add() {
-            if(this.isDisabledAdd || this.disabled) {
+            if (this.isDisabledAdd || this.disabled) {
                 return
             }
             let inputValue = Number(this.inputValue) + Number(this.step)
-            if(inputValue > this.max) {
+            if (this.max !== '' && inputValue > this.max) {
                 inputValue = this.max
             }
+            inputValue = inputValue.toFixed(Number(this.precision))
             this.inputValue = inputValue
             this.$emit('change', inputValue)
         },
@@ -146,13 +192,14 @@ export default {
          * @return
          */
         less() {
-            if(this.isDisabledLess || this.disabled) {
+            if (this.isDisabledLess || this.disabled) {
                 return
             }
             let inputValue = Number(this.inputValue) - Number(this.step)
-            if(inputValue < this.min) {
+            if (this.min !== '' && inputValue < this.min) {
                 inputValue = this.min
             }
+            inputValue = inputValue.toFixed(Number(this.precision))
             this.inputValue = inputValue
             this.$emit('change', inputValue)
         },
@@ -168,10 +215,9 @@ export default {
     watch: {
         value: {
             handler(newValue) {
-                
                 this.inputValue = newValue
             },
-            immediate: true
+            //immediate: true
         }
     }
 }
@@ -186,8 +232,9 @@ export default {
     border: 1px solid $cs_border_color;
     cursor: pointer;
     user-select: none;
-    transition: border-color .15s;
-    .cs-inputNumber-less-default, .cs-inputNumber-add-default {
+    transition: border-color 0.15s;
+    .cs-inputNumber-less-default,
+    .cs-inputNumber-add-default {
         float: left;
         line-height: 40px;
         width: 40px;
@@ -200,9 +247,58 @@ export default {
     .cs-inputNumber-less-default {
         border-right: 1px solid $cs_border_color;
     }
-    .cs-inputNumber-less-default-disabled, .cs-inputNumber-add-default-disabled {
+    .cs-inputNumber-less-default-disabled,
+    .cs-inputNumber-add-default-disabled {
         color: $color_disabled;
         cursor: not-allowed;
+    }
+    .cs-inputNumber-control-box {
+        line-height: 18px;
+        .cs-inputNumber-control-add,.cs-inputNumber-control-less {
+            width: 40px;
+        }
+        .cs-inputNumber-control-add {
+            border-bottom: 1px solid $cs_border_color;
+        }
+        i {
+            font-size: 12px;
+        }
+        .cs-inputNumber-control-large {
+            width: 100%;
+            line-height: 16px;
+            height: 18px;
+        }
+        .cs-inputNumber-control-small {
+            width: 100%;
+            line-height: 14px;
+            height: 16px;
+        }
+        .cs-inputNumber-control-mini {
+            width: 100%;
+            line-height: 12px;
+            height: 14px;
+        }
+    }
+    .cs-inputNumber-add-default-large {
+        width: 36px;
+    }
+    .cs-inputNumber-add-default-small {
+        width: 32px;
+    }
+    .cs-inputNumber-add-default-mini {
+        width: 28px;
+    }
+    .cs-inputNumber-control-large {
+        line-height: 36px;
+        width: 36px;
+    }
+    .cs-inputNumber-control-small {
+        line-height: 32px;
+        width: 32px;
+    }
+    .cs-inputNumber-control-mini {
+        line-height: 28px;
+        width: 28px;
     }
     .cs-inputNumber-add-default {
         float: right;
@@ -217,6 +313,19 @@ export default {
     }
     .cs-inputNumber-content-default {
         padding: 0 44px;
+    }
+    .cs-inputNumber-content-right {
+        padding-left: 0 !important;
+        padding-right: 44px;
+    }
+    .cs-inputNumber-content-large {
+        padding: 0 40px;
+    }
+    .cs-inputNumber-content-small {
+        padding: 0 36px;
+    }
+    .cs-inputNumber-content-mini {
+        padding: 0 32px;
     }
 }
 .cs-inputNumber:hover {
@@ -237,5 +346,14 @@ export default {
 }
 .cs-inputNumber-disabled:hover {
     border-color: $cs_border_color;
+}
+.cs-inputNumber-large {
+    width: 168px;
+}
+.cs-inputNumber-small {
+    width: 156px;
+}
+.cs-inputNumber-mini {
+    width: 144px;
 }
 </style>
--- src/page/inputNumber.vue
+++ src/page/inputNumber.vue
@@ -3,7 +3,35 @@
         <div class="cs-doc-demo-block">
             <div class="cs-doc-demo-block__title">基础用法</div>
             <div class="button-demo-content">
-                <cs-inputNumber v-model="basisValue" step="3"></cs-inputNumber>
+                <cs-inputNumber v-model="basisValue" step="1"></cs-inputNumber>
+            </div>
+        </div>
+
+        <div class="cs-doc-demo-block">
+            <div class="cs-doc-demo-block__title">精度</div>
+            <div class="button-demo-content">
+                <cs-inputNumber v-model="precisionValue" step="0.2" precision='1'></cs-inputNumber>
+            </div>
+        </div>
+
+        <div class="cs-doc-demo-block">
+            <div class="cs-doc-demo-block__title">尺寸</div>
+            <div class="button-demo-content">
+                <cs-inputNumber size="large" v-model="mediumValue" step="0.2" precision='1'></cs-inputNumber>
+
+                <cs-inputNumber size="small" v-model="mediumValue" step="0.2" precision='1'></cs-inputNumber>
+
+                <cs-inputNumber size="mini" v-model="mediumValue" step="0.2" precision='1'></cs-inputNumber>
+            </div>
+        </div>
+
+        <div class="cs-doc-demo-block">
+            <div class="cs-doc-demo-block__title">按钮位置</div>
+            <div class="button-demo-content">
+                <cs-inputNumber size="" v-model="rightValue" step="1" max="5" min="0" controlsPosition="right" ></cs-inputNumber>
+                <cs-inputNumber size="large" v-model="rightValue" step="1" max="5" min="0" controlsPosition="right" ></cs-inputNumber>
+                <cs-inputNumber size="small" v-model="rightValue" step="1" max="5" min="0" controlsPosition="right" ></cs-inputNumber>
+                <cs-inputNumber size="mini" v-model="rightValue" step="1" max="5" min="0" controlsPosition="right" ></cs-inputNumber>
             </div>
         </div>
     </div>
@@ -13,7 +41,10 @@
 export default {
     data() {
         return {
-            basisValue: '1'
+            basisValue: '1',
+            precisionValue: '1',
+            mediumValue: '1',
+            rightValue: '1'
         }
     }
 }
